9d344b12aa9440d1b7919d0cc6e607d45bcc5d47
David Whitlark <whitlark@gmail.com>
2015-06-28 15:58:15 -0400

    working through getting all ports & protections right

--- run.sh
+++ run.sh
@@ -2,34 +2,38 @@
 
 mkdir docker-data
 
+docker rm -f frontage
+docker run --name frontage \
+    -v "$PWD/docker-data:/etc/nginx/htpasswd" \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    -p 80:80 \
+    -d goldbuick/util-proxy
+
+docker rm -f ui-config
+docker run --name ui-config \
+    -e VIRTUAL_HOST=config.$1 \
+    -d goldbuick/ui-config
+
+docker rm -f ui-chat
+docker run --name ui-chat \
+    -e VIRTUAL_HOST=chat.$1 \
+    -d goldbuick/ui-chat
+
 docker rm -f rethinkdb
 docker run --name rethinkdb \
+    -e VIRTUAL_HOST=dive.$1 \
+    -e VIRTUAL_PORT=8080 \
     -v "$PWD/docker-data:/data" \
     -d rethinkdb
-
-docker rm -f private
-docker run --name private \
-    -v /var/run/docker.sock:/tmp/docker.sock:ro \
-    -d jwilder/nginx-proxy
 
 docker rm -f base
 docker run --name base \
     --link rethinkdb:rethinkdb \
-    -d goldbuick/stem-config-server
-
-docker rm -f terminal
-docker run --name terminal \
-    --link base:base \
-    -d goldbuick/stem-terminal-server
+    -d goldbuick/stem-base
 
 docker rm -f barrier
 docker run --name barrier \
-    --link private:private \
-    --link rethinkdb:rethinkdb \
     --link base:base \
-    --link terminal:terminal \
-    -p 8080:70 \
-    -p 8888:7080 \
     -p 7154:6154 \
     -p 26154:16154 \
     -v "$PWD/docker-data:/var/nginx" \
